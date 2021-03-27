0ef7cc1811b10c8ac3850c3fc3ac31b614899b26
HackBL <bojms45@gmail.com>
2021-03-27 19:25:23 -0400

    70

---
+++ Array/70. Climbing Stairs/Solution_1.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int climbStairs(int n) {
+        if (n <= 3) return n;
+        
+        int first = 2, second = 3, third = 0;
+        for (int i = 4; i <= n; i++) {
+            third = first + second;
+            first = second;
+            second = third;
+        }
+        
+        return third;
+    }
+}
