8f8578efe7da83a7abc3211a8901e8e765e5e9aa
HackBL <bojms45@gmail.com>
2021-03-27 22:25:20 -0400

    15

---
+++ Array/15. 3Sum/Solution_1.java
@@ -0,0 +1,31 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> res = new ArrayList();
+        Arrays.sort(nums);
+        
+        for (int i = 0; i < nums.length-2; i++) {
+            if (i == 0 || (i > 0 && nums[i] != nums[i-1])) {
+                int lo = i+1, hi = nums.length-1;
+                
+                while (lo < hi) {
+                    if (nums[i] + nums[lo] + nums[hi] == 0) {
+                        res.add(Arrays.asList(nums[i], nums[lo], nums[hi]));
+                        while (lo < hi && nums[lo] == nums[lo+1]) lo++;
+                        while (lo < hi && nums[hi] == nums[hi-1]) hi--;
+                        
+                        lo++; hi--;
+                    } else if (nums[i] + nums[lo] + nums[hi] < 0) {
+                        lo++;
+                    } else {
+                        hi--;
+                    }
+                    
+                }                    
+            }
+            
+            
+        }
+        
+        return res;
+    }
+}
